9081fb277eeaed06560cebb22a42f76097a6ecf3
shincyej <shincyej@gmail.com>
2025-01-03 21:11:30 -0500

    Commiting change to event_planinng.py

--- deeplearning.ai/event_planning/event_planning/event_planning.py
+++ deeplearning.ai/event_planning/event_planning/event_planning.py
@@ -17,3 +17,19 @@ openai_api_key = os.getenv("OPENAI_API_KEY")
 os.environ["OPENAI_MODEL_NAME"] = "gpt-3.5-turbo"
 os.environ["SERPER_API_KEY"] = os.getenv("SERPER_API_KEY")
 
+upport_agent = Agent(
+    role="Senior Support Representative",
+	goal="Be the most friendly and helpful "
+        "support representative in your team",
+	backstory=(
+		"You work at crewAI (https://crewai.com) and "
+        " are now working on providing "
+		"support to {customer}, a super important customer "
+        " for your company."
+		"You need to make sure that you provide the best support!"
+		"Make sure to provide full complete answers, "
+        " and make no assumptions."
+	),
+	allow_delegation=False,
+	verbose=True
+)
